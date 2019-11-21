f7cf00eebe889c3a81bdcb1622ca51f12ddc59f5
Albert Teonando Suhardi <170709523@students.uajy.ac.id>
2019-11-21 13:45:06 +0700

    Ganbatteeeee

---
+++ precache-manifest.0cc6ec3858c5a0098046d72fa0c90ed0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaee3ae32985836e4931",
+    "url": "/css/about.3b3c9ddb.css"
+  },
+  {
+    "revision": "dd054526eda4cebc2758",
+    "url": "/css/chunk-vendors.ec4de8ca.css"
+  },
+  {
+    "revision": "b5e11fff78a4fe12f88716aeac953213",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaee3ae32985836e4931",
+    "url": "/js/about.e6adea7e.js"
+  },
+  {
+    "revision": "a60b7fba4fc82c82307c",
+    "url": "/js/app.036ce0f0.js"
+  },
+  {
+    "revision": "dd054526eda4cebc2758",
+    "url": "/js/chunk-vendors.641da9d9.js"
+  },
+  {
+    "revision": "61bd2788ce562346bfc97158a5cd5c9a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
